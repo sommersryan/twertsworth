0022892c52bbb01f28dac0b7fdcc99bf37963cc5
Ryan Sommers <ryansommers@gmail.com>
2016-10-27 23:53:18 -0400

    Added basic API configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+#Set the following variables in your environment with the values from your twitter application at apps.twitter.com 
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
+
