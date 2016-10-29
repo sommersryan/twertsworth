cec4c3f7586d4e13ebc6fffdfa117e14539ad545
Ryan Sommers <ryansommers@gmail.com>
2016-10-29 17:06:26 -0400

    Added method for retrieving stream of bytes from image URL

--- vision.py
+++ vision.py
@@ -1,6 +1,18 @@
-import json
+import json, base64, urllib.request
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
-def buildRequest(image):
+def getImageContent(imageURL):
+	req = urllib.request.Request(imageURL)
+	with urllib.request.urlopen(req) as response:
+		return base64.b64encode(response.read())
+	
+def buildRequest(image, content=True):
+	if content: 
+		imageRequest = {'content' : image.decode('UTF-8')}
+	else:
+		imageRequest = {'source': {'gcsImageUri':image}}
+		
 	request = json.dumps(
 				{'requests':[
 								{
@@ -10,14 +22,9 @@ def buildRequest(image):
 											'type':'label_detection'
 										}
 									],
-									'image':
-									{
-										'source':
-										{
-											'gcsImageUri':image
-										}
-									}
+									'image': imageRequest
 								}
 							]
 				})
 	return request
+
