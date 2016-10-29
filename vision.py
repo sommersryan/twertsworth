c390d7fc46db77a8d34ad4f6120531bb44041063
Ryan Sommers <ryansommers@gmail.com>
2016-10-29 18:04:41 -0400

    Simplified method for retrieving image labels

--- vision.py
+++ vision.py
@@ -2,29 +2,27 @@ import json, base64, urllib.request
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-def getImageContent(imageURL):
+credentials = GoogleCredentials.get_application_default()
+service = discovery.build('vision', 'v1', credentials=credentials)
+
+def getLabels(imageURL):
 	req = urllib.request.Request(imageURL)
 	with urllib.request.urlopen(req) as response:
-		return base64.b64encode(response.read())
-	
-def buildRequest(image, content=True):
-	if content: 
-		imageRequest = {'content' : image.decode('UTF-8')}
-	else:
-		imageRequest = {'source': {'gcsImageUri':image}}
-		
-	request = json.dumps(
-				{'requests':[
+		image_content = base64.b64encode(response.read())
+		service_request = service.images().annotate(body={
+			'requests':[
 								{
-									'features':
-									[
+									'image':
 										{
-											'type':'label_detection'
-										}
-									],
-									'image': imageRequest
-								}
-							]
-				})
-	return request
-
+											'content': image_content.decode('UTF-8')
+										},
+									'features':
+										[
+											{
+												'type':'label_detection'
+											}
+										]}
+							]})
+		response = service_request.execute()
+		labels = [label['description'] for label in response['responses'][0]['labelAnnotations']]
+		return labels
