8805788ebf2d9296dba36681d0f8a55748013d5a
Ryan Sommers <ryansommers@gmail.com>
2016-11-05 01:23:17 -0400

    Formats the tweets more nicely

--- __init__.py
+++ __init__.py
@@ -15,18 +15,29 @@ import vision
 newReplies = tweet.checkReplies()
 mediaReplies = []
 
-for reply in newReplies: #just take the replies with pictures
+#Just take the replies with pictures
+
+for reply in newReplies: 
 	if reply.media:
 		mediaReplies.append(reply)
 
-toDoList = [tweet.getMediaURL(a) for a in mediaReplies] #make list of tuples with the necessary info for each reply
+#Make list of tuples with the necessary info for each reply
+		
+toDoList = [tweet.getMediaURL(a) for a in mediaReplies] 
 logging.info('Processing replies %s'," ".join([r[0] for r in toDoList]))
 
 for reply in toDoList:
-	random.shuffle(reply[2]) #going to randomize the order and just pick the first one
+
+	#Going to randomize the order and just pick the first one
+	
+	random.shuffle(reply[2])
 	logging.info('Using image %s for reply %s',reply[2][0],reply[0])
 	imageLabels = vision.getLabels(reply[2][0])
 	logging.info('Using labels %s',','.join(imageLabels[:2]))
 	tweetText = compose.writePoem(textModel,imageLabels[:2],reply[1])
-	tweet.replyTo(tweetText,int(reply[0])) #reply ID needs to be int
+	tweetCharList = list(tweetText)
+	tweetCharList[0] = tweetCharList[0].upper()
+	finalCharList = [w.replace(',','\r') for w in tweetCharList]
+	finalTweet = "".join(finalCharList)
+	tweet.replyTo(finalTweet,int(reply[0])) #Reply ID needs to be int
 	
