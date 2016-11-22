7de7baa66d4bd22a2424c0b0c08983d4d85183b3
Ryan Sommers <ryansommers@gmail.com>
2016-11-22 17:06:00 -0500

    Completed stanza assembly method

--- compose.py
+++ compose.py
@@ -3,35 +3,48 @@ from config import MIN_CHAR, MAX_CHAR, ATTEMPTS, ENCODING, EXCLUDE_WORDS
 
 def writePoem(textModel, seedWords, tweetPrefix):
 	modelKeys = list(textModel.chain.model)
-	tweetRoom = MAX_CHAR - len(tweetPrefix) #need to keep track of how many chars we have for the tweet
+	#track chars available for poem
+	tweetRoom = MAX_CHAR - len(tweetPrefix)
 	stanzaLength = int(tweetRoom / 2) - 1
 	random.shuffle(seedWords)
 	stanzas = []
-	while len(stanzas) < 2:
-		for word in seedWords:
-			if word in EXCLUDE_WORDS:
-				continue
-			logging.info('Trying %s',word)
-			results = [v for i, v in enumerate(modelKeys) if v[0] == word]
-			logging.info('Found %s model key pairs',len(results))
-			if results:
-				#shuffle the model key pairs so we don't always get the same seed for a poem from similar images
-				random.shuffle(results) 
-				for result in results:
-					logging.info('Trying the pair %s',' '.join(result))
-					stanza = textModel.make_short_sentence(stanzaLength, init_state=result)
-					logging.info('Text model responded: %s',stanza)
-					if stanza:
-						logging.info("Satisfied conditions in %s attempts",t)							
-						stanzas.append(formatLine(stanza))
-						break
-				break		
+	
+	for word in seedWords:
+		
+		if len(stanzas) == 2:
+			finalPoem = tweetPrefix + '\r'.join(stanzas)
+			return finalPoem
+		
+		if word in EXCLUDE_WORDS:
+			continue
+		
+		logging.info('Trying %s',word)
+		results = [v for i, v in enumerate(modelKeys) if v[0] == word]
+		logging.info('Found %s model key pairs',len(results))
+		
+		if results:
+			random.shuffle(results) 
+			
+			for result in results:
+				logging.info('Trying the pair %s',' '.join(result))
+				stanza = textModel.make_short_sentence(stanzaLength, init_state=result)
+				logging.info('Text model responded: %s',stanza)
+				if stanza:
+					logging.info("Satisfied conditions in %s attempts",t)							
+					stanzas.append(formatLine(stanza))
+					break		
 				logging.info('Could not use this key pair')
-	#DEVELOPMENT -- going to need to add some stuff here to avoid infinite loops
+	
+	if len(stanzas) == 1:
+		stanzaTwo = textModel.make_short_sentence(stanzaLength)
+		stanzas.append(formatLine(stanzaTwo))
+		finalPoem = tweetPrefix + '\r'.join(stanzas)
+		return finalPoem
+		
 	logging.info("None of the seed words worked")
 	genericPoem = textModel.make_short_sentence(tweetRoom)
-	finalPoem = formatLine(genericPoem)
-	return tweetPrefix + finalPoem
+	finalPoem = tweetPrefix + formatLine(genericPoem)
+	return finalPoem
 	
 def formatLine(line):
 	words = [list(a) for a in line.split()]
